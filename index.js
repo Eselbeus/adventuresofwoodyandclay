896681c3798e048cc3a559cad7e17c6d9a69d956
github username <github email address>
2019-08-15 03:52:29 -0400

    episode advancer event listener

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+let img;
+
 window.addEventListener("DOMContentLoaded", e => {
   console.log("wheeeeeeeee")
+
+  img = document.getElementById("image__comic");
+  console.log(img)
+  let episodeNo = img.src.slice(-5, -4);
+  console.log(episodeNo)
+  let episodeNext = parseInt(episodeNo)
+
+  img.addEventListener('click', e => {
+    episodeNext++;
+    img.src = `./images/Catan_World_ep${episodeNext}.jpg`;
+    console.log(img)
+  })
 });
